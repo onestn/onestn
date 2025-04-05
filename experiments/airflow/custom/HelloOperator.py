28f2d7d6a867f78685c01016d17af4e40af17acf
wonseokyang-de <deadline.yang@gmail.com>
2025-04-05 07:54:13 +0000

    feat: Add Airflow custom operator

---
+++ experiments/airflow/custom/HelloOperator.py
@@ -0,0 +1,13 @@
+from airflow.models.baseoperator import BaseOperator
+
+
+class HelloOperator(BaseOperator):
+    def __init__(self, name: str, **kwargs):
+        super().__init__(**kwargs)
+        self.name = name
+
+    def execute(self, context):
+        message = f"Hello, {self.name}"
+        print(message)
+
+        return message
