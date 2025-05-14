0ae9d6dd411c0e3c8fcbd77ffe0f039b63ad0ed6
wonseokyang-de <deadline.yang@gmail.com>
2025-05-14 17:44:40 +0900

    feat: Add new experiment project

---
+++ experiments/pydantic/validation_successful.py
@@ -0,0 +1,29 @@
+from datetime import datetime
+
+from pydantic import BaseModel, PositiveInt
+
+
+class User(BaseModel):
+    id: int
+    name: str = 'John Doe'
+    singup_ts: datetime | None
+    tastes: dict[str, PositiveInt]
+
+
+external_data = {
+    'id': 123,
+    'singup_ts': '2019-06-01 12:22',
+    'tastes': {
+        'wine': 9,
+        b'cheese': 7,
+        'cabbage': '1',
+    }
+}
+
+user = User(**external_data)
+
+print(user.id)
+#> 123
+
+print(user.model_dump())
+
