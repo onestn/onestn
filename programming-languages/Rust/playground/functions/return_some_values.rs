b0ee7e09365987deebe45b90ec352b1aebfdd4d2
JeongtakYang <deadline.yang@gmail.com>
2025-07-10 22:32:50 +0900

    feat: Add some example

---
+++ programming-languages/Rust/playground/functions/return_some_values.rs
@@ -0,0 +1,15 @@
+fn _some_func(_a: String) -> String {
+    "test".to_string()
+}
+
+fn add_values(num_1: i32, num_2: i32) -> i32 {
+    let added_value: i32 = num_1 + num_2; 
+
+    added_value
+}
+
+fn main() {
+    // println!(some_fu("Hello".to_string());
+
+    println!("{}", add_values(100, 120));
+}
