9a688c9688a00a212bceadfe32489816f498291b
onestn <onestone.yang@gmail.com>
2026-01-27 17:39:07 +0900

    study: Rust

---
+++ programming-languages/Rust/easy-rust/chapter5_collection_and_error_handling/question_oprator.rs
@@ -0,0 +1,17 @@
+use std::num::ParseIntError;
+
+fn parse_str(input: &str) -> Result<i32, ParseIntError> {
+    let parsed_number = input
+        .parse::<u16>()?.to_string()
+        .parse::<u32>()?.to_string()
+        .parse::<i32>()?;
+    Ok(parsed_number)
+}
+
+fn main() {
+    let str_vec = vec!["Seven", "8", "9.0", "nice", "6060"];
+    for item in str_vec {
+        let parsed = parse_str(item);
+        println!("{parsed:?}");
+    }
+}
